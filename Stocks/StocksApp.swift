6b788f124cc28f470d5ca6fb37a94a221b2218eb
Logan Blevins <logan.blevins@angel.com>
2024-06-16 14:06:58 -0500

    Initial Commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Logan Blevins on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
